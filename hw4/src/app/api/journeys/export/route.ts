d1b2bc4a9954a3aaac9856a0d9f76f3354a8e90d
tom <aa123593465@gmail.com>
2023-12-29 08:45:18 +0800

    feature: Custom addCalendar

--- hw4/src/app/api/journeys/export/route.ts
+++ hw4/src/app/api/journeys/export/route.ts
@@ -1,13 +1,12 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-import { ConsoleLogWriter, and, desc, eq, } from "drizzle-orm";
+import {and, eq } from "drizzle-orm";
 import Pusher from "pusher";
 
 import { db } from "@/db";
 import { plansTable, journeysTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
-import { resourceUsage } from "process";
-import { CalendarIcon } from "@radix-ui/react-icons";
+
 
 
 // export journey
@@ -21,7 +20,9 @@ export async function POST(req: NextRequest) {
     const token = session.user.token;
     console.log(token)
 
-    const [journeys] = await db
+
+
+    const journeys = await db
       .select({
         journeyId: journeysTable.displayId,
         title: journeysTable.title,
@@ -35,6 +36,21 @@ export async function POST(req: NextRequest) {
       // .orderBy(journeysTable.timestamp) //之後要加
       .execute();
 
+    console.log("jorneys",journeys)
+
+    const processedJourneys = journeys.map((journey) => {
+      return {
+        journeyId: journey.journeyId,
+        title: journey.title,
+        start: journey.start,
+        end: journey.end,
+        location: journey.location,
+        note: journey.note,
+      };
+    })
+
+    console.log("processedJourneys",processedJourneys)
+
     const [plan] = await db
     .select({
       name: plansTable.name,
@@ -43,10 +59,12 @@ export async function POST(req: NextRequest) {
     .from(plansTable)
     .where(and(eq(plansTable.displayId, planId)))
     .execute();
-  
+
+
+    console.log("plan",plan)
      // add calendar
     // See: https://developers.google.com/calendar/api/v3/reference/calendars/insert?hl=zh-tw
-  const addCalendar = async (token:string) => {
+  const addCalendar = async (token:string, title:string, description:string) => {
     const response = await fetch(
       "https://www.googleapis.com/calendar/v3/calendars",
       {
@@ -56,8 +74,8 @@ export async function POST(req: NextRequest) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          summary: "一起衝鋒",
-          description: "This calendar is added by Google Calendar API",
+          summary: title,
+          description: description,
         }),
       }
     );
@@ -76,7 +94,19 @@ export async function POST(req: NextRequest) {
   if (!token ) {
     throw new Error("Token is required");
   }
-
+  const eventData = {
+    summary: "washi o lan", // summary = title
+    start: {
+      dateTime: "2023-12-29T15:00:00", // Start time in RFC3339 format
+      timeZone: "Asia/Taipei", // Sets the current time zone
+    },
+    end: {
+      dateTime: "2023-12-29T19:00:00",
+      timeZone: "Asia/Taipei", // Sets the current time zone
+    },
+    location: "Taipei",
+    description: "I wanna pass Web programming", // note = description
+  };
   // a function to add an event to Google Calendar
   const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`, {
     method: "POST",
@@ -87,32 +117,33 @@ export async function POST(req: NextRequest) {
     body: JSON.stringify(eventDetails), // Make sure eventDetails are passed to the function
   });
 
-  // Check for response status
-  if (!response.ok) {
-    throw new Error(`Error add event: ${response.status}`);
-  }
+    // Check for response status
+    if (!response.ok) {
+      throw new Error(`Error add event: ${response.status}`);
+    }
 
-  const data = await response.json();
-  return data;
-    };
+    const data = await response.json();
+    return data;
+      };
+
     const eventData = {
       summary: "washi o lan", // summary = title
       start: {
-        dateTime: "2023-12-27T15:00:00", // Start time in RFC3339 format
+        dateTime: "2023-12-29T15:00:00", // Start time in RFC3339 format
         timeZone: "Asia/Taipei", // Sets the current time zone
       },
       end: {
-        dateTime: "2023-12-27T19:00:00",
+        dateTime: "2023-12-29T19:00:00",
         timeZone: "Asia/Taipei", // Sets the current time zone
       },
       location: "Taipei",
       description: "I wanna pass Web programming", // note = description
     };
 
-
     
-    const CalendarId = await addCalendar(token);
+    const CalendarId = await addCalendar(token, plan.name, plan.description);
     addEvent(token, eventData, CalendarId);
+    console.log("export complete")
 
     return NextResponse.json(
       {
